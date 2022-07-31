84506e3a5111e15b91e05571eb4970ad0cc9c99d
ibrahimovfuad <fuad.ibrahimov@live.com>
2022-07-31 16:36:00 +0400

    test form

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useCallback, useRef, useState } from 'react';
+import GoTrue from 'gotrue-js';
+
+const auth = new GoTrue({
+  APIUrl: 'https://btgz.dev/.netlify/functions/test',
+  audience: '',
+  setCookie: false,
+});
 
 export default function Home() {
+  const [email,setEmail] = useState('');
+  const [pass,setPass] = useState('')
+  const onSubmit = useCallback((event) => {
+    event.preventDefault();
+    
+    auth
+      .signup(email, pass)
+      .then((response) => console.log('Confirmation email sent', response))
+      .catch((error) => console.log("It's an error", error));
+  }, [email, pass]);
   return (
     <div className="container">
       <Head>
@@ -10,14 +28,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <form onSubmit={onSubmit}>
+        <div>
+          <input type="email" required name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div>
+          <input type="password" required name="password" value={pass} onChange={(e) => setPass(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   )
 }
